0bc4c7ebeee201f74d6bd7de22e5692becb9f073
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-01-12 19:58:54 +0530

    SQL For Data Analysis

---
+++ SQL/CodeBasics.sql
@@ -0,0 +1,14 @@
+/*year wise sales*/ 
+Select dt.year,Sum(sales_amount) as "Total Sales" from sales.transactions as t inner join sales.date as dt on t.order_date=dt.date group by dt.year;
+
+/*region wise sales*/
+Select m.markets_code,m.markets_name,SUM(t.sales_amount) as "Total Sales" from sales.markets as m inner join sales.transactions as t on m.markets_code=t.market_code group by m.markets_code order by Sum(t.sales_amount) ; 
+
+/*product wise sales above 1 crore*/
+Select p.product_code,sum(t.sales_amount) as "Total Sales" from sales.products as p inner join sales.transactions as t on p.product_code=t.product_code group by p.product_code having  sum(t.sales_amount)>10000000 order by sum(t.sales_amount); 
+
+/*Customer type wise sales*/
+Select c.customer_type,Sum(t.sales_amount) from sales.customers as c inner join sales.transactions as t on c.customer_code=t.customer_code  group by c.customer_type; 
+
+/*region wise distinct product count*/
+Select m.markets_name,count(distinct(product_code)) as "Dsitinct Products" from sales.transactions as t inner join sales.markets as m on m.markets_code=t.market_code group by m.markets_code order by count(distinct(product_code)) ;
