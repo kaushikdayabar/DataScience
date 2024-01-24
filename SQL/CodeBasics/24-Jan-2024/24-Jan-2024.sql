fee67d44e4404d166504d52f091058db25375970
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-01-24 20:50:11 +0530

    Add files via upload
    1.	Constraints:Not Null 
    2.	Auto_increment attribute generates unique id for new rows automatically or to generate unique id on an existing table use row_number() over().
    Syntax: create table <new table name> as Select *,Row_number() over() from <existing table name>;  
    3.Alter command to delete multiple columns

---
+++ SQL/CodeBasics/24-Jan-2024/24-Jan-2024.sql
@@ -0,0 +1,53 @@
+Select * from cleanedtransactions limit 5;
+
+/* transactions with sales amount less than 1 is removed*/
+Select count(*) from cleanedtransactions where sales_amount<=0;
+
+/*Duplicate transactions removed */
+Select distinct(currency) from cleanedtransactions;
+
+/*total number of transactions*/
+Select count(*) from cleanedtransactions;
+/*
+# count(*)
+'148395'
+*/
+
+/* no primary ID column for transactions table*/
+Alter table cleanedtransactions add column ID int;	
+
+/*generated unique column */
+create table cleanedTransactions1 as Select *,Row_number() over() as IDUnique from cleanedtransactions; 
+
+select * from cleanedtransactions1 limit 5;
+/*
+product_code, customer_code, market_code, order_date, sales_qty, sales_amount, currency, NormalizedSalesAmount, ID, IDTemp, IDUnique
+'Prod001', 'Cus001', 'Mark001', '2017-10-10', '100', '41241', 'INR\r', NULL, NULL, NULL, '1'
+'Prod002', 'Cus003', 'Mark003', '2018-04-06', '1', '875', 'INR\r', NULL, NULL, NULL, '2'
+'Prod002', 'Cus003', 'Mark003', '2018-04-11', '1', '583', 'INR\r', NULL, NULL, NULL, '3'
+'Prod002', 'Cus004', 'Mark003', '2018-06-18', '6', '7176', 'INR\r', NULL, NULL, NULL, '4'
+'Prod003', 'Cus005', 'Mark004', '2017-11-20', '59', '500', 'USD\r', NULL, NULL, NULL, '5'
+
+*/
+
+/*deleting useless columns */
+alter table cleanedtransactions1 drop ID ,drop IDTemp;
+
+select * from cleanedtransactions1 limit 5;   
+/*
+# product_code, customer_code, market_code, order_date, sales_qty, sales_amount, currency, NormalizedSalesAmount, IDUnique
+'Prod001', 'Cus001', 'Mark001', '2017-10-10', '100', '41241', 'INR\r', NULL, '1'
+'Prod002', 'Cus003', 'Mark003', '2018-04-06', '1', '875', 'INR\r', NULL, '2'
+'Prod002', 'Cus003', 'Mark003', '2018-04-11', '1', '583', 'INR\r', NULL, '3'
+'Prod002', 'Cus004', 'Mark003', '2018-06-18', '6', '7176', 'INR\r', NULL, '4'
+'Prod003', 'Cus005', 'Mark004', '2017-11-20', '59', '500', 'USD\r', NULL, '5'
+
+*/
+
+
+Create procedure UpdateNormalizedSalesAmount(Total int)
+begin
+set @i=0;
+repeat 
+
+update cleanedtransactions1 set NormalizedSalesAmount=
