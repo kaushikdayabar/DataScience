0c4a9f5eed3c80a8e1918d19bf4ee7b53c093122
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-10-02 14:43:46 +0530

    Create SeparateChaining.py

---
+++ CodeBasics/Python/DataStructures/SeparateChaining.py
@@ -0,0 +1,46 @@
+class customDictionary:
+
+    def __init__(self):
+        self.Max=10
+        self.arr=[[] for i in range(self.Max)]
+    
+    def getHash(self,key):
+        h=0
+        for i in key:
+            h+=ord(i)
+        
+        return h % self.Max
+    
+    def __setitem__(self,key,item):
+        ind=self.getHash(key)
+        varEnum=list(enumerate(self.arr[ind]))
+
+        for idx,element in varEnum:
+            if element[0]==key:
+                self.arr[ind][idx][1]=item
+                return
+        
+        self.arr[ind].append((key,item))
+
+        print(self.arr)
+    
+    def __getitem__(self,key):
+        ind=self.getHash(key)
+        varEnum=list(enumerate(self.arr[ind]))
+
+        for idx,element in varEnum:
+            if element[0]==key:
+                return element[1]
+
+        return "Element Not found"
+    
+    def __delitem__(self,key):
+        ind=self.getHash(key)
+        varEnum=list(enumerate(self.arr[ind]))
+
+        for idx,element in varEnum:
+            if element[0]==key:
+                self.arr[ind].pop(idx)
+        
+    
+        
