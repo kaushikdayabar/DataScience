ff670b9b5caa2e1a6dc2057aa6447a3533110385
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-10-05 21:35:44 +0530

    Add files via upload

---
+++ CodeBasics/Python/DataStructures/Queue.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+class Queue:
+
+    def __init__(self):
+        self.Queue1=deque()
+    
+    def enQueue(self,data):
+        self.Queue1.appendleft(data)
+        print(self.Queue1)
+    
+    def deQueue(self):
+        self.Queue1.pop()
+        print(self.Queue1)
+    
+    def size(self):
+        return len(self.Queue1)
+    
+    def isEmpty(self):
+        return len(self.Queue1)==0
+
+
+ob=Queue()
+
+print("Enqueue 1 , 2 and 3 ")
+ob.enQueue(1)
+ob.enQueue(2)
+ob.enQueue(3)
+
+print("Dequeue last 2 ")
+ob.deQueue()
+ob.deQueue()
+
+print("Size of Queue")
+print(ob.size())
+
+print("Is Empty ")
+print(ob.isEmpty())
+
+print("dequeue last 1")
+ob.deQueue()
+
+print("Size of Queue")
+print(ob.size())
+
+print("Is Empty ")
+print(ob.isEmpty())
+
