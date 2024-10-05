7e0aa2281a3ba828054faaa9457eaa78e1a5fecb
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-10-05 20:12:39 +0530

    Add files via upload

---
+++ CodeBasics/Python/DataStructures/Stack.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+#Instead of list , we are using deque because list uses dynamic array while deque uses doubly linked list hence we avoid dynamic memory allocation problem
+class Stack:
+
+    def __init__(self):
+        self.container=deque()
+    
+    def push(self,data):
+        self.container.append(data)
+        print(self.container)
+    
+    def pop(self):
+        self.container.pop()
+        print(self.container)
+    
+    def peek(self):
+        return self.container[-1]
+    
+    def isEmpty(self):
+        return len(self.container)==0
+    
+    def sizeOfStack(self):
+        return len(self.container)
+
+
+
+
+ob=Stack()
+
+print("pushing 45,46 and 47")
+ob.push(45)
+ob.push(46)
+ob.push(47)
+
+print("2 peek operations")
+print(ob.peek())
+print(ob.peek())
+
+print("2 pop operations")
+ob.pop()
+ob.pop()
+
+print("IsEmpty ")
+print(ob.isEmpty())
+
+print("Pop Operation ")
+ob.pop()
+
+print("IsEmpty ")
+print(ob.isEmpty())
+
+print("size")
+print(ob.sizeOfStack())
+
+print("push Operation")
+ob.push(48)
+
+print("size")
+print(ob.sizeOfStack())
