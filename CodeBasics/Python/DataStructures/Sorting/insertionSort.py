1536adb8f60aa471b6625884a6005f9472adbcb5
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2024-10-19 15:35:00 +0530

    Update insertionSort.py
    Used Assignment operator instead of swapping

--- CodeBasics/Python/DataStructures/Sorting/insertionSort.py
+++ CodeBasics/Python/DataStructures/Sorting/insertionSort.py
@@ -1,16 +1,15 @@
 def insertionSort(l):
-
     n=len(l)
     for i in range(n):
         j=i-1
-        while j>-1:
+        anchor=l[i]
+        while j>-1 and anchor<l[j]:
             #print(f"i={i} j={j} l[i]={l[i]} l[j]= {l[j]}")
-            if l[j+1]<l[j]:
-                l[j+1],l[j]=l[j],l[j+1]
-                #print(l)
-            else:
-                break
+            l[j+1]=l[j]
+            #print(l)
             j-=1
+        l[j+1]=anchor
+        
     return l
 
 tests = [
